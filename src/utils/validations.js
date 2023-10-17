0fbc32acf5f5b20ce1761798b3c7f4a94ce4af0d
Salyens <salyens@yandex.ru>
2023-10-17 10:25:53 +0400

    update

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,16 +1,20 @@
 const validName = (name) => {
+  const trimmedName = name.trim();
   const regex = new RegExp(/^[a-z]+(?:\s[a-z]+)*$/i);
-  return regex.test(name);
+  return regex.test(trimmedName);
 };
 
 const validEmail = (email) => {
+  const trimmedEmail = email.trim();
   const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-  return regex.test(email);
+  return regex.test(trimmedEmail);
 };
 
 const validPassword = (password) => {
+  const trimmedPassword = password.trim();
   const regex = new RegExp(/^.{1,}$/);
-  return regex.test(password);
+  return regex.test(trimmedPassword);
 };
 
 module.exports = { validName, validEmail, validPassword };
+
